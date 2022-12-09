96991229a8c78c004c6d21fc34fa50b623797fa1
Staszek Pasko <staszek@gmail.com>
2022-12-09 13:03:03 +0100

    show part 1 & 2

--- vis/vis09.cs
+++ vis/vis09.cs
@@ -10,7 +10,8 @@ namespace aoc2022 {
             solver.parse(input);
         }
 
-        HashSet<int> visited = new HashSet<int>();
+        HashSet<int> visited1 = new HashSet<int>();
+        HashSet<int> visited2 = new HashSet<int>();
         List<Day09.Snake> segments = new List<Day09.Snake>();
         int pos = 0;
         int speed = 1;
@@ -18,12 +19,17 @@ namespace aoc2022 {
         public bool render(int idx) {
             if (speed < 5 && speed <= (idx / 1000)) speed++;
             for (int k = 0; pos < solver.data.Length && k < speed; k++, pos += 2) segments[0].move(solver.data[pos + 1], solver.data[pos]);
-            foreach (int p in visited) {
+            foreach (int p in visited1) {
+                int cy = p / 1000, cx = p % 1000;
+                DrawRectangle(cx * 3 - 500, cy * 3 - 100, 4, 4, DARKGRAY);
+            }
+            foreach (int p in visited2) {
                 int cy = p / 1000, cx = p % 1000;
                 DrawRectangle(cx * 3 - 500, cy * 3 - 100, 4, 4, LIGHTGRAY);
             }
             foreach (var seg in segments) DrawCircle(seg.cx * 3 - 500, seg.cy * 3 - 100, 3, RAYWHITE);
-            renderer.WriteXY(2,2,"Visited: " + visited.Count);
+            renderer.WriteXY(2,1,"Visited [1]: " + visited1.Count);
+            renderer.WriteXY(2,2,"Visited [2]: " + visited2.Count);
             renderer.WriteXY(2,3,"Speed: " + speed + " cells/frame");
             renderer.WriteXY(2,4,"Moves: " + pos + " / " + solver.data.Length);
             return (idx > solver.data.Length / 5);
@@ -35,8 +41,10 @@ namespace aoc2022 {
 
         public string part2() {
             Console.WriteLine(solver.data.Length);
-            segments.Add(new Day09.Snake(null, (x, y) => visited.Add(y * 1000 + x)));
-            for (int i = 0; i < 9; i++) segments.Add(new Day09.Snake(segments[segments.Count - 1]));
+            segments.Add(new Day09.Snake(null, (x, y) => visited2.Add(y * 1000 + x)));
+            for (int i = 0; i < 7; i++) segments.Add(new Day09.Snake(segments[segments.Count - 1]));
+            segments.Add(new Day09.Snake(segments[segments.Count - 1], (x, y) => visited1.Add(y * 1000 + x)));
+            segments.Add(new Day09.Snake(segments[segments.Count - 1]));
             segments.Reverse();
             renderer.loop(x => render(x));
             return solver.part2();
