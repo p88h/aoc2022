61ccfe0c7b94d3bde369ef024877ecf7f7f4c219
Staszek Pasko <staszek@gmail.com>
2022-12-11 12:39:30 +0100

    refactor

--- lib/day11.cs
+++ lib/day11.cs
@@ -1,65 +1,58 @@
 namespace aoc2022 {
     public class Day11 : Solution {
-
         public class Monkey {
-            public long divisor, a, b, c, count;
+            public long divisor, a, b, c;
             public int left, right;
-            public List<long> items = new List<long>(), init;
-            public Monkey(List<string> block) {
+            public Monkey(long pa, long pb, long pc, long d, int l, int r) {
+                a = pa; b = pb; c = pc; divisor = d; left = l; right = r;
+            }
+        }
+
+        public List<Monkey> monkeys = new List<Monkey>();
+        public List<(int, long)> items = new List<(int, long)>();
+        public long modulus = 1;
+
+        public void parse(List<string> input) {
+            for (int i = 0; i < input.Count; i += 7) {
+                var block = input.GetRange(i, 6);
                 var parts = block[1].Split(':');
-                init = parts[1].Split(',').Select(x => long.Parse(x)).ToList();
+                var init = parts[1].Split(',').Select(x => long.Parse(x)).ToList();
                 parts = block[2].Split(' ');
+                long a = 0, b = 0, c = 0;
                 if (parts[7] == "old") {
                     a = 1;
                 } else {
                     long v = long.Parse(parts[7]);
-                    if (parts[6] == "*") b = v; else c = v;
+                    if (parts[6] == "*") b = v; else { b = 1; c = v; }
                 }
-                divisor = long.Parse(block[3].Split(' ')[5]);
-                left = int.Parse(block[4].Split(' ')[9]);
-                right = int.Parse(block[5].Split(' ')[9]);
+                long d = long.Parse(block[3].Split(' ')[5]);
+                int l = int.Parse(block[4].Split(' ')[9]);
+                int r = int.Parse(block[5].Split(' ')[9]);
+                monkeys.Add(new Monkey(a, b, c, d, l, r));
+                foreach (var v in init) items.Add((i / 7, v));
+                modulus *= d;
             }
-            public void Reset() { items.Clear(); items.AddRange(init); count = 0; }
         }
 
-        public List<Monkey> monkeys = new List<Monkey>();
-        public long modulus = 1;
-
-        public void parse(List<string> input) {
-            for (int b = 0; b < input.Count; b += 7) monkeys.Add(new Monkey(input.GetRange(b, 6)));
-            foreach (var m in monkeys) modulus *= m.divisor;
-        }
-
-        public string part1() {
-            foreach (var m in monkeys) m.Reset();
-            for (int round = 0; round < 20; round++) {
-                foreach (var m in monkeys) {
-                    foreach (long v in m.items) {
-                        m.count++;
-                        long w = (m.a * v * v + m.b * v + m.c) / 3;
-                        monkeys[(w % m.divisor == 0) ? m.left : m.right].items.Add(w);
-                    }
-                    m.items.Clear();
+        public string run(int rounds, Func<long, long> worry) {
+            int[] count = new int[monkeys.Count];
+            foreach (var (i, v) in items) {
+                int c = i; long x = v;
+                for (int r = 0; r < rounds;) {
+                    var m = monkeys[c];
+                    count[c]++;
+                    x = worry(m.a * x * x + m.b * x + m.c);
+                    int d = (x % m.divisor == 0) ? m.left : m.right;
+                    if (d < c) r++;
+                    c = d;
                 }
             }
-            var top = monkeys.OrderByDescending(m => m.count).ToList();
-            return (top[0].count * top[1].count).ToString();
+            var top = count.OrderByDescending(n => n).ToList();
+            return (top[0] * top[1]).ToString();
         }
 
-        public string part2() {
-            foreach (var m in monkeys) m.Reset();
-            for (int round = 0; round < 10000; round++) {
-                foreach (var m in monkeys) {
-                    foreach (long v in m.items) {
-                        m.count++;
-                        long w = (m.a * v * v + m.b * v + m.c) % modulus;
-                        monkeys[(w % m.divisor == 0) ? m.left : m.right].items.Add(w);
-                    }
-                    m.items.Clear();
-                }
-            }
-            var top = monkeys.OrderByDescending(m => m.count).ToList();
-            return (top[0].count * top[1].count).ToString();
-        }
+        public string part1() { return run(20, (x) => x / 3); }
+
+        public string part2() { return run(10000, (x) => x % modulus); }
     }
 }
