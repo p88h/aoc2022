bdeb889f50bfe5b51aefea720321c78e3d5b9b17
Staszek Pasko <staszek@gmail.com>
2022-12-21 10:05:47 +0100

    cleanup

--- vis/vis20.cs
+++ vis/vis20.cs
@@ -1,6 +1,5 @@
 namespace aoc2022 {
     public class Vis20 : Solution {
-        private string data = "";
         private ASCIIRay renderer = new ASCIIRay(1920, 1080, 60, 32, "Day20");
         private Day20 solver = new Day20();
         public void parse(List<string> input) {
@@ -11,23 +10,23 @@ namespace aoc2022 {
             var order = new List<(long, long)>(solver.data.GetRange(0, 500));
             var temp = new Day20.BucketList<(long, long)>(order);
             int idx = -1, shift = 0, len = order.Count;
-            HashSet<(long,long)> done = new HashSet<(long, long)>();
+            HashSet<(long, long)> done = new HashSet<(long, long)>();
             int slow = 10, totswaps = 0, totshifts = 0, balcnt = 0;
             renderer.loop(cnt => {
                 if (shift == 0) {
-                    if (idx >= 0) done.Add(order[idx]);                    
+                    if (idx >= 0) done.Add(order[idx]);
                     if (idx == len - 1) return true;
                     var n = order[++idx];
-                    shift = (int) (n.Item2 >= 0 ? n.Item2 % (len - 1) : (len - 1) - ((-n.Item2) % (len - 1)));
+                    shift = (int)(n.Item2 >= 0 ? n.Item2 % (len - 1) : (len - 1) - ((-n.Item2) % (len - 1)));
                     totshifts += shift;
                     if (idx > 0 && idx % temp.size == 0) {
-                        balcnt ++;
+                        balcnt++;
                         temp = new Day20.BucketList<(long, long)>(temp);
                     }
                 } else if (cnt % slow == 0) {
                     int b = temp.idx[order[idx]];
                     int nb = (b + 1) % temp.buckets.Count;
-                    if (temp.buckets[b][temp.buckets[b].Count-1]==order[idx] && temp.buckets[nb].Count > 0 && shift > 0) {
+                    if (temp.buckets[b][temp.buckets[b].Count - 1] == order[idx] && temp.buckets[nb].Count > 0 && shift > 0) {
                         shift -= temp.buckets[nb].Count;
                         temp.Shift(order[idx], temp.buckets[nb].Count);
                     } else {
@@ -48,19 +47,19 @@ namespace aoc2022 {
                                                                            totswaps, totshifts, balcnt));
                 for (int b = 0; b < temp.buckets.Count; b++) {
                     renderer.SetColor(240, 200, 200, 255);
-                    renderer.WriteXY(0,b,tot + "-" + (tot + temp.buckets[b].Count) + ": ");
+                    renderer.WriteXY(0, b, tot + "-" + (tot + temp.buckets[b].Count) + ": ");
                     renderer.SetColor(120, 120, 160, 255);
                     tot += temp.buckets[b].Count;
                     for (int i = 0; i < temp.buckets[b].Count; i++) {
                         if (temp.buckets[b][i] == order[idx]) {
                             renderer.SetColor(200, 250, 200, 255);
-                            renderer.WriteXY(12+i*6,b,temp.buckets[b][i].Item2.ToString());                            
+                            renderer.WriteXY(12 + i * 6, b, temp.buckets[b][i].Item2.ToString());
                         } else if (done.Contains(temp.buckets[b][i])) {
                             renderer.SetColor(60, 60, 160, 255);
-                            renderer.WriteXY(12+i*6,b,temp.buckets[b][i].Item2.ToString());
+                            renderer.WriteXY(12 + i * 6, b, temp.buckets[b][i].Item2.ToString());
                         } else {
                             renderer.SetColor(120, 80, 120, 255);
-                            renderer.WriteXY(12+i*6,b,temp.buckets[b][i].Item2.ToString());
+                            renderer.WriteXY(12 + i * 6, b, temp.buckets[b][i].Item2.ToString());
                         }
                     }
                 }
